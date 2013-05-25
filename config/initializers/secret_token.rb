cca2740f50b48709cb18cca1c4b5f8eee2b5ad12
Geethu Jerry <geethumarias@gmail.com>
2013-05-25 15:04:51 +0800

    Initial commit for rubyonrails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apps::Application.config.secret_token = '0accea3715de6bbbd457232a32a474ebd62c543cfd5efec70fcf8e666fae09b158156c263dc1696f2f40965887df2ed0092f70ac89063d36ba18fd751988a3ca'
